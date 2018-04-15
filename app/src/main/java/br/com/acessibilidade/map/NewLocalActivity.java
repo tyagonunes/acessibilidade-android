1578528af90274871364007909f7480293be7f4a
Tyago <tyagonunes@gmail.com>
2018-04-14 23:41:50 -0300

    auto complete

--- app/src/main/java/br/com/acessibilidade/map/NewLocalActivity.java
+++ app/src/main/java/br/com/acessibilidade/map/NewLocalActivity.java
@@ -1,13 +1,34 @@
 package br.com.acessibilidade.map;
 
 import android.os.Bundle;
+import android.support.annotation.NonNull;
 import android.support.design.widget.FloatingActionButton;
 import android.support.design.widget.Snackbar;
 import android.support.v7.app.AppCompatActivity;
 import android.support.v7.widget.Toolbar;
+import android.util.Log;
 import android.view.View;
+import android.widget.EditText;
+import android.widget.TextView;
 
-public class NewLocalActivity extends AppCompatActivity {
+import com.google.android.gms.common.ConnectionResult;
+import com.google.android.gms.common.api.GoogleApiClient;
+import com.google.android.gms.common.api.Status;
+import com.google.android.gms.location.places.Place;
+import com.google.android.gms.location.places.Places;
+import com.google.android.gms.location.places.ui.PlaceAutocompleteFragment;
+import com.google.android.gms.location.places.ui.PlaceSelectionListener;
+
+import br.com.acessibilidade.map.models.Local;
+
+public class NewLocalActivity extends AppCompatActivity implements PlaceSelectionListener,
+        GoogleApiClient.OnConnectionFailedListener {
+
+    private GoogleApiClient mGoogleApiClient;
+    private EditText local_name;
+    private Local local;
+    private Double latitude;
+    private Double longitude;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -17,6 +38,50 @@ public class NewLocalActivity extends AppCompatActivity {
         setSupportActionBar(toolbar);
 
         getSupportActionBar().setDisplayHomeAsUpEnabled(true);
+
+        mGoogleApiClient = new GoogleApiClient
+                .Builder(this)
+                .addApi(Places.GEO_DATA_API)
+                .addApi(Places.PLACE_DETECTION_API)
+                .enableAutoManage(this, this)
+                .build();
+
+        PlaceAutocompleteFragment autocompleteFragment = (PlaceAutocompleteFragment)
+                getFragmentManager().findFragmentById(R.id.place_autocomplete_fragment);
+
+        autocompleteFragment.setOnPlaceSelectedListener(this);
+
+        local_name = (EditText) findViewById(R.id.local_name);
     }
 
+
+    @Override
+    public void onPlaceSelected(Place place) {
+        Log.d("Successo", "Place Selected: " + place.getLatLng().latitude);
+        local_name.setText(place.getName());
+        //latitude = place.getLatLng().latitude;
+        //longitude = place.getLatLng().longitude;
+
+
+
+        //send();
+    }
+
+    @Override
+    public void onError(Status status) {
+
+    }
+
+    @Override
+    public void onConnectionFailed(@NonNull ConnectionResult connectionResult) {
+
+    }
+
+    public void send() {
+      //  local.setNome(place.getName());
+        local.setLatitude(latitude);
+        local.setLongitude(longitude);
+
+        Log.d("Local", local.toString());
+    }
 }
